87b761aa099de90e059855dfd2e491014bfe9031
Camila <camvives@gmail.com>
2021-06-24 10:24:21 -0300

    Rectángulo más grande y lectura dataset

--- Code/main.py
+++ Code/main.py
@@ -4,19 +4,17 @@ import cv2
 face_locations = []
 face_encodings = []
 
-video_capture = cv2.VideoCapture("img\pexels-mikhail-nilov-7735486.mp4")
+video_capture = cv2.VideoCapture("img\pexels-kampus-production-8348919.mp4")
 success, frame = video_capture.read()
 cv2.imwrite("imagenes\\frame_extraido.jpg", frame)
 
-
 small_frame = cv2.resize(frame, (0, 0), fx=0.25, fy=0.25)
-
 
 face_locations = face_recognition.face_locations(small_frame)
 face_encodings = face_recognition.face_encodings(small_frame, face_locations)
 
 for top, right, bottom, left in face_locations:
-    cv2.rectangle(small_frame, (left, top), (right, bottom), (0, 0, 255), 2)
+    cv2.rectangle(small_frame, (left-15, top-35), (right+15, bottom+15), (0, 0, 255), 2)
 
 cv2.imshow('Video', small_frame)
 cv2.waitKey()
---
+++ Code/training_model.py
@@ -0,0 +1,30 @@
+from imutils import paths
+import os
+import numpy as np
+
+from tensorflow.keras.applications.mobilenet_v2 import preprocess_input
+
+from tensorflow.keras.preprocessing.image import img_to_array
+from tensorflow.keras.preprocessing.image import load_img
+from sklearn.preprocessing import LabelBinarizer
+
+dataset=r"C:\\Users\\camvi\Documents\Soporte\\TPI\\Mask Dataset"
+imagePaths=list(paths.list_images(dataset))
+
+data = []
+labels = []
+
+for im in imagePaths:
+    label=im.split(os.path.sep)[-2]
+    labels.append(label)
+    image=load_img(im,target_size=(224,224))
+    image=img_to_array(image)
+    image=preprocess_input(image) #RGB a BGR
+    data.append(image)
+
+#Convierte los datos a arrays
+data=np.array(data,dtype="float32")
+labels=np.array(labels)
+
+
+print(labels)
